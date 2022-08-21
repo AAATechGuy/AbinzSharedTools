a0bb7d4aa3e86ef6345097eb632e2800b2b45d14
Abin Abraham Alichan <abinaa@microsoft.com>
2022-08-21 01:02:55 -0700

    nit update ABTests

--- src/ABTests/ExternalLogic.cs
+++ src/ABTests/ExternalLogic.cs
@@ -1,75 +1,74 @@
-﻿namespace External
+﻿namespace External;
+
+public class ExternalLogic
 {
-    public class ExternalLogic
+    public static CampaignType Method1(ImportCampaign ic, Campaign c, bool isLocalorBulkFormatLocalImport, ImportType importType)
     {
-        public static CampaignType Method1(ImportCampaign ic, Campaign c, bool isLocalorBulkFormatLocalImport, ImportType importType)
-        {
-            return ic.Type != null && c != null && c.CampaignType == CampaignType.Search &&
-                ic.Type == ImportCampaignType.DynamicSearchAds && c.ServerId != null
-                    ? (CampaignType)(byte)ic.Type
-                    : (isLocalorBulkFormatLocalImport || importType == ImportType.UpgradeServerImport) && c != null
-                        ? c.ServerId == null && ic.Type != null
-                            ? (CampaignType)(byte)ic.Type
-                            : c.CampaignType
-                        : (CampaignType)(byte)(ic.Type ?? ImportCampaignType.Search);
-        }
-
-        public static CampaignType Method2(ImportCampaign ic, Campaign c, bool isLocalorBulkFormatLocalImport, ImportType importType)
-        {
-            return ic.Type != null && c != null && //c.CampaignType == CampaignType.Search &&
-                ic.Type == ImportCampaignType.DynamicSearchAds && c.ServerId != null
-                    ? (CampaignType)(byte)ic.Type
-                    : (isLocalorBulkFormatLocalImport || importType == ImportType.UpgradeServerImport) && c != null
-                        ? c.ServerId == null && ic.Type != null
-                            ? (CampaignType)(byte)ic.Type
-                            : c.CampaignType
-                        : (CampaignType)(byte)(ic.Type ?? ImportCampaignType.Search);
-        }
+        return ic.Type != null && c != null && c.CampaignType == CampaignType.Search &&
+            ic.Type == ImportCampaignType.DynamicSearchAds && c.ServerId != null
+                ? (CampaignType)(byte)ic.Type
+                : (isLocalorBulkFormatLocalImport || importType == ImportType.UpgradeServerImport) && c != null
+                    ? c.ServerId == null && ic.Type != null
+                        ? (CampaignType)(byte)ic.Type
+                        : c.CampaignType
+                    : (CampaignType)(byte)(ic.Type ?? ImportCampaignType.Search);
     }
 
-    #region ObjectModel
-
-    public enum CampaignType
+    public static CampaignType Method2(ImportCampaign ic, Campaign c, bool isLocalorBulkFormatLocalImport, ImportType importType)
     {
-        Shopping = 1,
-        DynamicSearchAds = 2,
-        Search = 3,
-        Audience = 4,
-        SmartShopping = 5,
-        AudienceShopping = 6
+        return ic.Type != null && c != null && //c.CampaignType == CampaignType.Search &&
+            ic.Type == ImportCampaignType.DynamicSearchAds && c.ServerId != null
+                ? (CampaignType)(byte)ic.Type
+                : (isLocalorBulkFormatLocalImport || importType == ImportType.UpgradeServerImport) && c != null
+                    ? c.ServerId == null && ic.Type != null
+                        ? (CampaignType)(byte)ic.Type
+                        : c.CampaignType
+                    : (CampaignType)(byte)(ic.Type ?? ImportCampaignType.Search);
     }
-
-    public enum ImportCampaignType
-    {
-        SearchAndContent = 0,
-        Shopping = 1,
-        DynamicSearchAds = 2,
-        Search = 3,
-        Audience = 4,
-        SmartShopping = 5,
-        AudienceShopping = 6
-    }
-
-    public enum ImportType
-    {
-        LocalImport = 0,
-        GetChanges = 1,
-        GetChangesAfterPost = 2,
-        UpgradeServerImport = 3,
-        GetFullAccountDownload = 4,
-        LocalImportBulkFormat = 5
-    }
-
-    public class ImportCampaign
-    {
-        public ImportCampaignType? Type { get; set; }
-    }
-
-    public class Campaign
-    {
-        public long? ServerId { get; set; }
-        public CampaignType CampaignType { get; set; }
-    }
-
-    #endregion ObjectModel
 }
+
+#region ObjectModel
+
+public enum CampaignType
+{
+    Shopping = 1,
+    DynamicSearchAds = 2,
+    Search = 3,
+    Audience = 4,
+    SmartShopping = 5,
+    AudienceShopping = 6
+}
+
+public enum ImportCampaignType
+{
+    SearchAndContent = 0,
+    Shopping = 1,
+    DynamicSearchAds = 2,
+    Search = 3,
+    Audience = 4,
+    SmartShopping = 5,
+    AudienceShopping = 6
+}
+
+public enum ImportType
+{
+    LocalImport = 0,
+    GetChanges = 1,
+    GetChangesAfterPost = 2,
+    UpgradeServerImport = 3,
+    GetFullAccountDownload = 4,
+    LocalImportBulkFormat = 5
+}
+
+public class ImportCampaign
+{
+    public ImportCampaignType? Type { get; set; }
+}
+
+public class Campaign
+{
+    public long? ServerId { get; set; }
+    public CampaignType CampaignType { get; set; }
+}
+
+#endregion ObjectModel
--- src/ABTests/Tests.cs
+++ src/ABTests/Tests.cs
@@ -6,68 +6,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ABTests
+namespace ABTests;
+
+[TestClass]
+public class Tests
 {
-    [TestClass]
-    public class Tests
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
+        int failedCount = 0;
+        foreach (var input in GetTestObjects(maxObjectsToGenerate: 1000))
         {
-            int failedCount = 0;
-            foreach (var input in GetTestObjects(maxObjectsToGenerate: 1000))
+            var abTestResult = ObjectABTester.RunABTest<MethodParam, CampaignType>(
+                input.MethodInput,
+                methodParam => ExternalLogic.Method1(methodParam.ic, methodParam.c, methodParam.isLocalorBulkFormatLocalImport, methodParam.importType),
+                methodParam => ExternalLogic.Method2(methodParam.ic, methodParam.c, methodParam.isLocalorBulkFormatLocalImport, methodParam.importType),
+                resultComparer: (resultA, resultB) => resultA == resultB,
+                exComparer: (exA, exB) => exA?.GetType() == exB?.GetType() && exA?.Message == exB?.Message);
+
+            if (abTestResult != null)
             {
-                var abTestResult = ObjectABTester.RunABTest<MethodParam, CampaignType>(
-                    input.MethodInput,
-                    methodParam => ExternalLogic.Method1(methodParam.ic, methodParam.c, methodParam.isLocalorBulkFormatLocalImport, methodParam.importType),
-                    methodParam => ExternalLogic.Method2(methodParam.ic, methodParam.c, methodParam.isLocalorBulkFormatLocalImport, methodParam.importType),
-                    resultComparer: (resultA, resultB) => resultA == resultB,
-                    exComparer: (exA, exB) => exA?.GetType() == exB?.GetType() && exA?.Message == exB?.Message);
-
-                if (abTestResult != null)
-                {
-                    Console.WriteLine($"[{DateTime.UtcNow.ToString("u")}][#{input.IterationId}] {abTestResult.MismatchType}: {JsonConvert.SerializeObject(input.MethodInput, Formatting.Indented)}");
-                    ++failedCount;
-                }
-
-                if (input.IterationId % 100 == 0)
-                {
-                    Console.WriteLine($"[{DateTime.UtcNow.ToString("u")}][#{input.IterationId}] ..");
-                }
+                Console.WriteLine($"[{DateTime.UtcNow.ToString("u")}][#{input.IterationId}] {abTestResult.MismatchType}: {JsonConvert.SerializeObject(input.MethodInput, Formatting.Indented)}");
+                ++failedCount;
             }
 
-            if (failedCount > 0)
+            if (input.IterationId % 100 == 0)
             {
-                Assert.Fail($"failedCount={failedCount}");
+                Console.WriteLine($"[{DateTime.UtcNow.ToString("u")}][#{input.IterationId}] ..");
             }
         }
 
-        private static IEnumerable<(int IterationId, MethodParam MethodInput)> GetTestObjects(int maxObjectsToGenerate)
+        if (failedCount > 0)
         {
-            //Set the randomizer seed if you wish to generate repeatable data sets.
-            Randomizer.Seed = new Random(8675309);
-
-            var testImportCampaignsGen = new Faker<ImportCampaign>()
-                .RuleFor(ic => ic.Type, f => f.PickRandom<ImportCampaignType>().OrNull(f, 0.8f));
-            var testCampaignsGen = new Faker<Campaign>()
-                .RuleFor(c => c.ServerId, f => f.Random.Long(-1, 2))
-                .RuleFor(ic => ic.CampaignType, f => f.PickRandom<CampaignType>());
-            var testMethodParamsGen = new Faker<MethodParam>()
-                .RuleFor(p => p.ic, f => testImportCampaignsGen.Generate())
-                .RuleFor(p => p.c, f => testCampaignsGen.Generate())
-                .RuleFor(p => p.isLocalorBulkFormatLocalImport, f => f.Random.Bool())
-                .RuleFor(p => p.importType, f => f.PickRandom<ImportType>());
-
-            return Enumerable.Range(0, maxObjectsToGenerate)
-                .Select(iterationId => (iterationId, testMethodParamsGen.Generate()));
+            Assert.Fail($"failedCount={failedCount}");
         }
+    }
 
-        public class MethodParam
-        {
-            public ImportCampaign ic;
-            public Campaign c;
-            public bool isLocalorBulkFormatLocalImport;
-            public ImportType importType;
-        }
+    private static IEnumerable<(int IterationId, MethodParam MethodInput)> GetTestObjects(int maxObjectsToGenerate)
+    {
+        //Set the randomizer seed if you wish to generate repeatable data sets.
+        Randomizer.Seed = new Random(8675309);
+
+        var testImportCampaignsGen = new Faker<ImportCampaign>()
+            .RuleFor(ic => ic.Type, f => f.PickRandom<ImportCampaignType>().OrNull(f, 0.8f));
+        var testCampaignsGen = new Faker<Campaign>()
+            .RuleFor(c => c.ServerId, f => f.Random.Long(-1, 2))
+            .RuleFor(ic => ic.CampaignType, f => f.PickRandom<CampaignType>());
+        var testMethodParamsGen = new Faker<MethodParam>()
+            .RuleFor(p => p.ic, f => testImportCampaignsGen.Generate())
+            .RuleFor(p => p.c, f => testCampaignsGen.Generate())
+            .RuleFor(p => p.isLocalorBulkFormatLocalImport, f => f.Random.Bool())
+            .RuleFor(p => p.importType, f => f.PickRandom<ImportType>());
+
+        return Enumerable.Range(0, maxObjectsToGenerate)
+            .Select(iterationId => (iterationId, testMethodParamsGen.Generate()));
+    }
+
+    public class MethodParam
+    {
+        public ImportCampaign ic;
+        public Campaign c;
+        public bool isLocalorBulkFormatLocalImport;
+        public ImportType importType;
     }
 }
